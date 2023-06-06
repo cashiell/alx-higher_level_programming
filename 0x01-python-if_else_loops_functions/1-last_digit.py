5f79ffaa3b1c2100ee16e9d767b8f8f6bbc88ed8
cashiell <cashiell.sekori@gmail.com>
2023-06-06 11:19:31 -0700

    1-last_digit.py

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -4,7 +4,7 @@ number = random.randint(-10000, 10000)
 if number >= 0:
     last_digit = number % 10
 else:
-    least_digit = ((number * -1) % 10) * -1
+    last_digit = ((number * -1) % 10) * -1
 
 message = "Last digit of %d is %d and is" % (number, last_digit)
 if last_digit == 0:

0ebb93339668b3b3b23e5a7bc8a728b8f3eaf20c
cashiell <cashiell.sekori@gmail.com>
2023-06-08 10:25:02 -0700

    1-calculation.py

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
+    """Print addition, substraction, multiplecation and quotient of 5 and 10."""
     from calculator_1 import add, sub, mul, div
     a = 10
     b = 5
